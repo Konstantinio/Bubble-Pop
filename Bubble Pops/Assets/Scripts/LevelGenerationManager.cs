1a7b61a42d58c43d2c211a0367f0a732af417782
Константин Марунченко <kostya.marunchenko@gmail.com>
2019-11-23 14:33:42 +0200

    Basic level generation

---
+++ Bubble Pops/Assets/Scripts/LevelGenerationManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerationManager : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public GameObject parent;
+    public Vector2 startPosition;
+    public float xGap;
+    public float yGap;
+    private int lineLength = 6;
+    private int startHeight = 5;
+
+    private void Start()
+    {
+        GenerateBasicLevel();
+    }
+
+    private void GenerateBasicLevel()
+    {
+        for (int y = 0; y < startHeight; y++)
+        {
+            for (int x = 0; x < lineLength; x++)
+            {
+                Instantiate(ballPrefab, startPosition+new Vector2(x*xGap + y%2*xGap/2,-y*yGap), Quaternion.identity, parent.transform);
+            }
+        }
+    }
+}
