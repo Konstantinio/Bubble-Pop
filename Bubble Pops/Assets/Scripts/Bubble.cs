966f61196acffb9189f5d3a75b35a9848e8a311c
Константин Марунченко <kostya.marunchenko@gmail.com>
2019-11-24 20:36:52 +0200

    Fixed end bubbles searching near bubbles not properly

--- Bubble Pops/Assets/Scripts/Bubble.cs
+++ Bubble Pops/Assets/Scripts/Bubble.cs
@@ -10,7 +10,9 @@ public class Bubble : MonoBehaviour
     public int indexer;
     public float maxDistance;
     public bool isShowingRedLines;
+
     public bool isGhost;
+
     //private float timer = 0.1f;
     private void Start()
     {
@@ -21,48 +23,57 @@ public class Bubble : MonoBehaviour
     {
         InitializeBubble();
         //timer -= Time.deltaTime;
-       // if (timer <= 0)
-       // {
-       //     Destroy(gameObject);
-       // }
+        // if (timer <= 0)
+        // {
+        //     Destroy(gameObject);
+        // }
     }
 
     private void InitializeBubble()
     {
         Vector2 p = transform.position;
-        nearBubbles[0] = ShootRay(p, Vector2.left + Vector2.up);
-       nearBubbles[1] = ShootRay(p,Vector2.right+Vector2.up);
-       nearBubbles[2] = ShootRay(p,Vector2.right);
-       nearBubbles[3] = ShootRay(p,Vector2.right+Vector2.down);
-       nearBubbles[4] = ShootRay(p,Vector2.left+Vector2.down);
-       nearBubbles[5] = ShootRay(p,Vector2.left);
+        SetNearBubble(0, ShootRay(p, Vector2.left + Vector2.up));
+        SetNearBubble(1, ShootRay(p, Vector2.right + Vector2.up));
+        SetNearBubble(2, ShootRay(p, Vector2.right));
+        SetNearBubble(3, ShootRay(p, Vector2.right + Vector2.down));
+        SetNearBubble(4, ShootRay(p, Vector2.left + Vector2.down));
+        SetNearBubble(5, ShootRay(p, Vector2.left));
 
-       for (int i = 0; i < 6; i++)
-       {
-           if (nearBubbles[i] != null)
-           {
-               if (i <= 2)
-               {
-                   nearBubbles[i].nearBubbles[i + 3] = this;
-               }
-               else
-               {
-                   nearBubbles[i].nearBubbles[i - 3] = this;
-               }
-           }
-       }
+        for (int i = 0; i < 6; i++)
+        {
+            if (nearBubbles[i] != null)
+            {
+                if (i <= 2)
+                {
+                    nearBubbles[i].nearBubbles[i + 3] = this;
+                }
+                else
+                {
+                    nearBubbles[i].nearBubbles[i - 3] = this;
+                }
+            }
+        }
     }
+
+    private void SetNearBubble(int index, Bubble bubbleToSet)
+    {
+        if (nearBubbles[index] == null)
+        {
+            nearBubbles[index] = bubbleToSet;
+        }
+    }
+
 
     [CanBeNull]
     private Bubble ShootRay(Vector2 startPosition, Vector2 endPosition)
     {
-        var hit = Physics2D.Raycast(startPosition, endPosition, maxDistance);
-        if (hit.collider != null && hit.collider.gameObject.GetComponent<Bubble>()!=null)
+        var hit = Physics2D.Raycast(startPosition, endPosition, maxDistance,~(1<<10));
+        if (hit.collider != null && hit.collider.gameObject.GetComponent<Bubble>() != null)
         {
             var bubble = hit.collider.gameObject.GetComponent<Bubble>();
-            Debug.DrawLine(startPosition, hit.point, (bubble.indexer == 1 && this.indexer==1) ? Color.yellow : Color.blue);
+            Debug.DrawLine(startPosition, hit.point,
+                (bubble.indexer == 1 && this.indexer == 1) ? Color.yellow : Color.blue);
             return bubble;
-            
         }
         else
         {
@@ -70,10 +81,8 @@ public class Bubble : MonoBehaviour
             {
                 Debug.DrawRay(startPosition, endPosition * 1000, Color.red);
             }
-            
+
             return null;
-            
         }
-        
     }
 }
