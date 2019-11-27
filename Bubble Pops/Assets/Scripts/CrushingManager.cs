796896260bbabc5b28fcc5aa45771e743fe827b8
Константин Марунченко <kostya.marunchenko@gmail.com>
2019-11-27 16:00:27 +0200

    Bug fixing

--- Bubble Pops/Assets/Scripts/CrushingManager.cs
+++ Bubble Pops/Assets/Scripts/CrushingManager.cs
@@ -8,30 +8,33 @@ public class CrushingManager : MonoBehaviour
 {
     public List<Fly> CrushingBubbles;
     public ColorConfiguration ColorConfiguration;
+    private Fly highest;
 
     public void Crush()
     {
         if (CrushingBubbles.Count >= 2)
         {
            
-            var highest = CrushingBubbles.OrderByDescending(x => x.transform.position.y).ToArray()[0];
-            Debug.Log(highest.GetComponent<Bubble>().indexer);
+            highest = CrushingBubbles.OrderByDescending(x => x.transform.position.y).ToArray()[0];
             highest.GetComponent<Crush>().isHighest = true;
             var position = (Vector2)highest.transform.position;
-            highest.GetComponent<Bubble>().SetIndexer(highest.GetComponent<Bubble>().indexer*(int)Math.Pow(2,CrushingBubbles.Count-1));
-            highest.GetComponent<SpriteRenderer>().color = ColorConfiguration.colorPowers
-                .FirstOrDefault(x => x.indexer == highest.GetComponent<Bubble>().indexer).color;
-            CrushingBubbles.ForEach(x => x.GetComponent<Crush>().Fly(position));
-            
-            foreach (var bubble in CrushingBubbles)
-            {
-                if (bubble != highest)
-                {
-                    Destroy(bubble.gameObject);
-                }
-            }
+          //  highest.GetComponent<Bubble>().SetIndexer(highest.GetComponent<Bubble>().indexer*(int)Math.Pow(2,CrushingBubbles.Count-1));
+           // highest.GetComponent<SpriteRenderer>().color = ColorConfiguration.colorPowers
+             //   .FirstOrDefault(x => x.indexer == highest.GetComponent<Bubble>().indexer).color;
+         
+             
+           var newBubble =  Instantiate(GetComponent<LevelGenerationManager>().bubblePrefab);
+           newBubble.transform.position = position;
+           newBubble.GetComponent<Bubble>().SetIndexer(highest.GetComponent<Bubble>().indexer*(int)Math.Pow(2,CrushingBubbles.Count-1));
+           newBubble.GetComponent<SpriteRenderer>().color=ColorConfiguration.colorPowers
+               .FirstOrDefault(x => x.indexer == newBubble.GetComponent<Bubble>().indexer).color;
+           CrushingBubbles.ForEach(x => x.GetComponent<Crush>().Fly(position));
             CrushingBubbles = new List<Fly>();
+            
+          
+          
         }
        
     }
+    
 }
